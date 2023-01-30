3d307780680143f2f16d6352e618007b1dc81f4e
Ethan Zhang <zhangeth@usc.edu>
2023-01-29 18:10:21 -0800

    meet the team pics done

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -20,52 +20,132 @@ function About() {
             <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
 
 
-            
             </a>
             <div class="container">
                 <div class="row">
-                <div>
-                    <h2 class="lh-1">Meet the Team</h2>
+                    <div>
+                        <h2 class="lh-1">Meet the Team</h2>
+                    </div>
                 </div>
+
+                <div class="row py-0">
                     <div class="col">
                         <figure class="image is-256x256" width="100%" height="100%">
                             <img src={YiAnn} width="100%" height="100%"/>
                         </figure>
-                        
-                        <figure width="100%" height="100%">
-                            <img src={Chris} width="100%" height="100%"/>
-                        </figure>
-                        <figure width="100%" height="100%">
-                            <img src={Peony} width="100%" height="100%"/>
-                        </figure>
                     </div>
-
                     <div class="col">
                         <figure width="100%" height="100%">
                             <img src={Audrey} width="100%" height="100%"/>
                         </figure>
-                        <figure width="100%" height="100%">
-                            <img src={Riya} width="100%" height="100%"/>
-                        </figure>
-                        <figure width="100%" height="100%">
-                            <img src={Ethan} width="100%" height="100%"/>
-                        </figure>
                     </div>
-
                     <div class="col">
-                        <figure class="image is-256x256" width="100%" height="100%">
+                        <figure width="100%" height="100%">
                             <img src={Mindy} width="100%" height="100%"/>
                         </figure>
-                        <figure width="100%" height="100%">
-                            <img src={Bryan} width="100%" height="100%"/>
+                    </div>
+                </div>
+
+                <div class="row py-0">
+                    <div class="col">
+                        <h5> Chapter President </h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Yi-Ann Li </p>
+                    </div>
+                    <div class="col">
+                        <h5> Co-President</h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Audrey Xia </p>
+                    </div>
+                    <div class="col">
+                        <h5> Chapter President </h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Mindy </p>
+                    </div>
+                </div>
+                    
+                <div class="row py-0">
+                    <div class="col">
+                        <figure class="image is-256x256" width="100%" height="100%">
+                            <img src={Chris} width="100%" height="100%"/>
                         </figure>
+                    </div>
+                    <div class="col">
                         <figure width="100%" height="100%">
                             <img src={Stephanie} width="100%" height="100%"/>
                         </figure>
                     </div>
+                    <div class="col">
+                        <figure width="100%" height="100%">
+                            <img src={Riya} width="100%" height="100%"/>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="row py-0">
+                    <div class="col">
+                        <h5> Chapter President </h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Yi-Ann Li </p>
+                    </div>
+                    <div class="col">
+                        <h5> Co-President</h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Audrey Xia </p>
+                    </div>
+                    <div class="col">
+                        <h5> Chapter President </h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Mindy </p>
+                    </div>
+               </div>
+
+               <div class="row py-0">
+                    <div class="col">
+                        <figure class="image is-256x256" width="100%" height="100%">
+                            <img src={Peony} width="100%" height="100%"/>
+                        </figure>
+                    </div>
+                    <div class="col">
+                        <figure width="100%" height="100%">
+                            <img src={Bryan} width="100%" height="100%"/>
+                        </figure>
+                    </div>
+                    <div class="col">
+                        <figure width="100%" height="100%">
+                            <img src={Ethan} width="100%" height="100%"/>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="row py-0">
+                    <div class="col">
+                        <h5> Chapter President </h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Yi-Ann Li </p>
+                    </div>
+                    <div class="col">
+                        <h5> Co-President</h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Audrey Xia </p>
+                    </div>
+                    <div class="col">
+                        <h5> Chapter President </h5>
+                    </div>
+                    <div class="col" style={{'textAlign':'right'}}>
+                        <p> Mindy </p>
+                    </div>
                 </div>
             </div>
-        </div>
+            </div>
 	);
 }
 export default About;
