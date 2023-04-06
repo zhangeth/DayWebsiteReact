28b56ff2d9ed30924161a44bd8973ef4d4f79579
Joseph Caluya <jcaluya@usc.edu>
2023-04-06 02:55:42 -0700

    made generalized literature containers

---
+++ src/components/litComponents/litContainer.jsx
@@ -0,0 +1,44 @@
+function LiteratureContainer(title, author, excerpt, image, imageCredit, page)
+{
+    return(
+        <>
+
+            <div class="row literature-container">
+                <div class="col-6 p-0">
+                    <div>
+                        <h4 class="py-1" style={{textAlign:'center', color: 'black'}}>{title}</h4>
+                        <h4 class="pb-2" style={{textAlign:'center', color: 'black'}}>{author}</h4>
+                    </div>
+                    <div class="px-3 py-2">
+                        <h5 style={{textAlign:'center', color:'black'}}>
+                            {excerpt}
+                        </h5>
+                    </div>
+                    <div class="px-3 py-4" style={{textAlign:'center'}}>
+                        <a class="btn btn-outline-danger" href={page} role="button">Continue Reading</a>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <figure class="image is-256x100" width="100%" height="100%">
+                        <img src={image} width="100%" height="100%"/>
+                        <p style={{fontSize:'x-small'}}>
+                            {imageCredit}
+                        </p>
+                    </figure>
+                </div>
+                <style jsx>{`
+                    .literature-container {
+                        display: flex;
+                        align-items: stretch;
+                    }
+                    .col-6:first-child {
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-between;
+                    }
+                `}</style>
+            </div>
+        </>
+    );
+}
+export default LiteratureContainer;
