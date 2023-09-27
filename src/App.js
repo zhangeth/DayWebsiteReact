380ccbb85238d4ae40deec3f102841133b7b781c
Ethan Zhang <ezhan@MacBook-Air.attlocal.net>
2023-09-26 23:08:09 -0500

    navbar and header dynamically resize, header/navbar styling into css, deleted old folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Navbar from "./components/Navbar/navbar";
-import Title from "./components/Title/title";
 import Footer from "./components/footer";
 
 import './css/globalComponents.css';
@@ -7,16 +5,11 @@ import './css/globalComponents.css';
 import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
-import {listArticleModels,syncArticleModels, syncAuthorModels} from './graphql/queries';
-
 import React, { useEffect, useState} from "react";
 import { BrowserRouter as Router, Routes, Route, Link, Outlet} from 'react-router-dom';
 
-import renderArticle from "./components/renderArticle";
-
 import Home from "./pages/home"
 import About from "./pages/about"
-import Art from "./pages/art"
  
 // article pages for now
 import Colonialism from "./pages/colonialism";
@@ -41,36 +34,41 @@ function scrollFunction() {
 
   if (goingDown) {
     document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("navbar").style.top = "-50px";
+
   } else {
     document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("navbar").style.top = "108px";
   }
 
 }
 
 Amplify.configure(awsconfig);
 
-// TODO: include <Route path="/art" element={<Art />} />
 const App = () => {
   return (
     <Router>
-    <div id="header">DAY USC</div>
-    <div style={{ paddingTop: '150px' }}></div>
+    <h1 id="header"> DAY USC </h1>
 
+    <div id="navbar">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </div>
 
-    {Navbar()}
+    <div style={{'padding': '80px'}}></div>
     
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/colonialism" element={<Colonialism />} />
-        <Route path="/monterey" element={<Monterey />} />
-        <Route path="/kpop" element={<Kpop />} />
-        <Route path="/fashion" element={<Fashion />} />
-        <Route path="/halmoni" element={<Halmoni />} />
-      </Routes>
-      {Footer()}
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/colonialism" element={<Colonialism />} />
+      <Route path="/monterey" element={<Monterey />} />
+      <Route path="/kpop" element={<Kpop />} />
+      <Route path="/fashion" element={<Fashion />} />
+      <Route path="/halmoni" element={<Halmoni />} />
+    </Routes>
 
+    {Footer()}
+    </Router>
   );
 }
 
