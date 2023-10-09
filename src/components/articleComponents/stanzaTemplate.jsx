7b1a1d43b31acb6d6cc59cc28d412eec0a22d606
Joseph Caluya <jcaluya@usc.edu>
2023-10-08 21:25:12 -0700

    deleted public fonts and old articles

--- src/components/articleComponents/stanzaTemplate.jsx
+++ src/components/articleComponents/stanzaTemplate.jsx
@@ -1,13 +1,13 @@
 // for poem stanzas
 import '../../css/article.css'
 
-function stanzaTemplate(stanzas){
+function stanzaTemplate(stanza){
     const stanzaElements = [];
 
-    for (let i = 0; i < stanzas.length; i++) {
+    for (let i = 0; i < stanza.length; i++) {
         stanzaElements.push(
             <div key={i}>
-                <p className="article-stanza">{stanzas[i]}</p>
+                <p className="article-stanza">{stanza[i]}</p>
             </div>
         );
     }
