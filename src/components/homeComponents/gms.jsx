bc61ffb1d2977d9cbbe6909cb0c297796768c5ed
Eric Chen <ericsc@usc.edu>
2024-03-17 23:05:38 -0700

    events updates

---
+++ src/components/homeComponents/gms.jsx
@@ -0,0 +1,25 @@
+function gms(type, gmArray, gmName, gmLink){
+    const allGMs = [];
+
+    for(let i = 0; i < 3; i++){
+        allGMs.push(
+            <div className="col-sm-12 col-md-4">
+                <div className="imgBox">
+                <a href={gmLink[i]}>
+                    <img src={gmArray[i]} alt={gmName[i]}/>
+                </a>
+        </div>  
+            </div>
+        )
+    }
+
+    return(
+        <div className={type}>
+        <h2 className="row text-center">{type}</h2>
+        <p className="row"> Tuesdays, from 7 - 8 pm. Follow us on instagram @dearasianyouth.usc for more information </p>
+        <div className="row justify-content-center">{allGMs}</div>
+    </div>        
+    )
+}
+
+export default gms;
